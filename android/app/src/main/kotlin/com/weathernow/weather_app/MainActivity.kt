fb63fd48c4d9d852e606d0c0010cd57073b38df7
Divansh Mittal <mittaldivansh@gmail.com>
2024-11-29 11:16:20 +0530

    Initial commit for Weather Now

---
+++ android/app/src/main/kotlin/com/weathernow/weather_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.weathernow.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/api_key.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'c3b1da7ad1ac9e90d8c340a37d302d2d';
